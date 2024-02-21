21b7353727c0c363e6c26d933d8b9306548c51ce
Miguel Solano <mesolano@intellisys.com.do>
2024-02-21 08:41:23 -0400

    Adding Github Actions and test coverage

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testConcurrency: 1,
-    apiKey: 'MyAPIKey',
+    apiKey: '6jWzpYsFi7WapkYi8Umd102QC1TB7ESUTCpR2UGoea107Zg110',
     batchName: 'TAU Todo App',
     browser: [
         {width: 800, height: 600, name: 'chrome'},
